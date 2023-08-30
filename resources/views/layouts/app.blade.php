895c604d164afa26fc7acf002ecbc8bdde18f65f
Eligio Biasi <ex_98@live.it>
2023-08-30 22:25:12 +0200

    added link to create new project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,10 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a href="{{route('admin.project.create')}}" class="nav-link d-inline-block">
+                                    Create your project
+                                </a>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle d-inline-block" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
