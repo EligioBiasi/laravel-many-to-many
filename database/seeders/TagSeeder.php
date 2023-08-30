a3ea338cb9a4938919c2da2da76aed8cd412a845
Eligio Biasi <ex_98@live.it>
2023-08-30 16:12:00 +0200

    defined seeder for tag

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TagSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $tags=[
+            'completed','work in progress','indie','casual'
+        ];
+
+        foreach ($tags as $tag) {
+            $newtag= new Tag();
+            $newtag->name=$tag;
+            $newtag->color=$faker->safeHexColor();
+            $newtag->save();
+        }
     }
 }
